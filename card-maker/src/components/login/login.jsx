7b88740f2c90640adf2a2aa11e107474098e4639
Jaeyah <seongjae9611@daum.net>
2021-06-16 04:41:12 +0900

    login and logout functionality

--- card-maker/src/components/login/login.jsx
+++ card-maker/src/components/login/login.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../header/header';
 import Footer from '../footer/footer';
 import styles from './login.module.css';
+import { useHistory } from 'react-router-dom';
 
 const Login = ({authService}) => {
-    const onLogin = event => {
-        authService.login(event.currentTarget.textContent).then(console.log);
+    const history =useHistory();
+    const goToMaker = userId => {
+        history.push({
+            pathname: '/maker',
+            state: {id: userId},
+        });
     };
+
+    const onLogin = event => {
+        authService
+        .login(event.currentTarget.textContent)
+        .then(data => goToMaker(data.user.uid));
+    };
+
+    useEffect(() => {
+        authService
+        .onAuthChange(user => {
+            user && goToMaker(user.uid);
+        });
+    });
+
     return (
         <section className={styles.login}>
             <Header />
